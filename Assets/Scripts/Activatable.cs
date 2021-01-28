d2360468448b219e266404c7f1e2ac1b51f4c051
Krzysztof Staniszewski <krzysiek199720@gmail.com>
2021-01-28 15:08:18 +0100

    quick fix

--- Assets/Scripts/Activatable.cs
+++ Assets/Scripts/Activatable.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public abstract class Activatable : MonoBehaviour
 {
-    public abstract void Action();
+    public virtual void Action()
+    {
+        Debug.LogError("Method not implemented");
+    }
     public virtual void Action(PlayerController playerController)
     {
         Action();
