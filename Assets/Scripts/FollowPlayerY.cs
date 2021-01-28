d5790351f39c3dacd67ed2cf26478b3c1ca543b4
Krzysztof Staniszewski <krzysiek199720@gmail.com>
2021-01-28 10:12:52 +0100

    basic movement, camera foollow basic

---
+++ Assets/Scripts/FollowPlayerY.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerY : MonoBehaviour
+{
+    public Transform playerTransform;
+    
+    void Update()
+    {
+        float playerY = playerTransform.position.y;
+        transform.position = new Vector3(
+            transform.position.x,
+            playerY,
+            transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMoveController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveController : MonoBehaviour
+{
+    public float speed = 1f;
+
+
+    private Vector2 desiredMovement = Vector2.zero;
+    private float speedModifier = 1f;
+
+    private Rigidbody2D rb2d;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        if (rb2d == null)
+            Debug.LogError("No Rigidbody2D found");
+    }
+
+    void FixedUpdate()
+    {
+        // smooth axis for now, can change to GetAxisRaw
+        desiredMovement = new Vector2(Input.GetAxis("Horizontal"), speed * speedModifier);
+
+        rb2d.MovePosition(rb2d.position + desiredMovement * Time.fixedDeltaTime);
+    }
+}
