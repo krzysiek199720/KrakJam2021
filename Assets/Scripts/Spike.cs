350a3533a4667c29b827d3312245859b0f9e5a3e
Krzysztof Staniszewski <krzysiek199720@gmail.com>
2021-01-29 14:21:38 +0100

    ultimate destroys obstacles

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Spike : Activatable
 {
     public SpikeData spikeData;
-    
-     public override void Action(PlayerController playerController)
+
+    private Tilemap tilemap;
+
+    private bool toDestroy = false;
+
+    private void Start()
+    {
+        tilemap = GetComponentInParent<Tilemap>();
+    }
+
+    private void Update()
+    {
+        // just to avoid error messages
+        if (toDestroy)
+        {
+            tilemap.SetTile(tilemap.WorldToCell(transform.position), null);
+        }
+    }
+
+    public override void Action(PlayerController playerController)
     {
         Debug.Log("Spike");
         if (playerController.isUltimateActive)
+        {
+            if (tilemap != null)
+            {
+                toDestroy = true;
+            }
             return;
+        }
 
         if (playerController.isGhost)
             return;
