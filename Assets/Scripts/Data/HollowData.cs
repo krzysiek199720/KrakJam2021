a37d5f56fcc4d0375d6b385f3c69582f667f5412
Krzysztof Staniszewski <krzysiek199720@gmail.com>
2021-01-29 17:26:39 +0100

    Obstacle hollow

---
+++ Assets/Scripts/Data/HollowData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "HollowData", menuName = "ScriptableObject/Obstacles/Hollow")]
+public class HollowData : ScriptableObject
+{
+    public int damage = 1;
+}
+
---
+++ Assets/Scripts/Hollow.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Hollow : Activatable
+{
+    public HollowData hollowData;
+
+    private Tilemap tilemap;
+
+    private bool toDestroy = false;
+
+    private void Start()
+    {
+        tilemap = GetComponentInParent<Tilemap>();
+    }
+
+    private void Update()
+    {
+        // just to avoid error messages
+        if (toDestroy)
+        {
+            tilemap.SetTile(tilemap.WorldToCell(transform.position), null);
+        }
+    }
+
+    public override void Action(PlayerController playerController)
+    {
+        if (playerController.isUltimateActive)
+        {
+            if (tilemap != null)
+            {
+                toDestroy = true;
+            }
+            return;
+        }
+
+        if (playerController.isGhost)
+            return;
+
+
+        GameManager.Instance.TakeLifeline(hollowData.damage);
+    }
+}
