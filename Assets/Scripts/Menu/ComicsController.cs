4fd5febecb241ba940906739869842f3d3bdab8e
Mateusz Wdowiak <55950450+matija00@users.noreply.github.com>
2021-01-30 14:41:33 +0100

    scene load

---
+++ Assets/Scripts/Menu/ComicsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ComicsController : MonoBehaviour
+{
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
