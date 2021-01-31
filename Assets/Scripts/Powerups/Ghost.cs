796e54c107b9fd24c6cf4e7deb861927b43ab002
Mateusz Wdowiak <55950450+matija00@users.noreply.github.com>
2021-01-31 09:27:13 +0100

    final

--- Assets/Scripts/Powerups/Ghost.cs
+++ Assets/Scripts/Powerups/Ghost.cs
@@ -9,19 +9,15 @@ public class Ghost : Powerup
     public override void PowerupStart(PlayerController playerController)
     {
         playerController.isGhost = true;
-        
-        Color tmp = playerController.GetComponent<SpriteRenderer>().color;
-        tmp.a = 0.5f;
-        playerController.GetComponent<SpriteRenderer>().color = tmp;
+
+        playerController.animator.SetBool("isGhost", true);
     }
 
     public override void PowerupEnd(PlayerController playerController)
     {
         playerController.isGhost = false;
 
-        Color tmp = playerController.GetComponent<SpriteRenderer>().color;
-        tmp.a = 1f;
-        playerController.GetComponent<SpriteRenderer>().color = tmp;
+        playerController.animator.SetBool("isGhost", false);
     }
 
     public override void AddTime()
